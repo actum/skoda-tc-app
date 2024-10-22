9f6a46b91ac43877e0d14c285691fd34b6afcd90
bajza <radek.zika@dipcom.cz>
2024-10-22 17:12:38 +0200

    button

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native';
+import ButtonsExample from '@/src/components/button/ButtonsExample';
 
 export default function HomeScreen() {
   return (
     <View style={{ paddingTop: 50 }}>
       <Text style={{ fontSize: 30 }}> HOME </Text>
+      <ButtonsExample />
     </View>
   );
 }
---
+++ src/components/button/ButtonsExample.tsx
@@ -0,0 +1,9 @@
+import { Button, Text, View } from 'react-native';
+
+export default function ButtonsExample() {
+  return (
+    <View style={{ paddingTop: 50 }}>
+      <Button title={'TEST'} />
+    </View>
+  );
+}
